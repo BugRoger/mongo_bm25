efcaee1e15510afcff97e6bc416fea996f145cc8
Michael Schmidt <michael.j.schmidt@gmail.com>
2011-03-03 18:34:12 +0100

    remove unwanted files

--- bin/ranking/avgDocLength_map.js
+++
@@ -1,4 +0,0 @@
-function() {
-  emit("length", this.length);
-}    
-
--- bin/ranking/nqi_map.js
+++
@@ -1,7 +0,0 @@
-function() {
-    this.tf.forEach(
-      function(z) {
-        emit(z.t, { count: 1 } );
-      }
-    )
-}    
